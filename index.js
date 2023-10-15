187af96212ba7af9c8c3666daf284f9770ed2d0c
i-fateh <fateh180581@gmail.com>
2023-10-15 10:31:24 +0200

    add new image

--- index.js
+++ index.js
@@ -1,12 +1,27 @@
 let blurry = document.getElementById('blurry')
-let myScore = document.getElementById('myScore')
+let myChoice = document.getElementById('myChoice')
 let illustration = document.getElementById('illustration')
+let surprise = document.getElementById('surprise')
 
-myScore.addEventListener('change', () => {
-    console.log(myScore.value);
-        blurry.style.backdropFilter = `blur(${parseInt(myScore.value)}px)`
-        if(myScore.value < 10){
-            illustration.src = "./img/clown.png"
+let body = document.getElementById('body')
+let head = document.getElementById('head')
+let par = document.getElementById('par')
+let container = document.getElementById('container')
+
+
+
+
+surprise.style.display = 'none'
+
+myChoice.addEventListener('change', () => {
+    console.log(myChoice.value);
+        blurry.style.backdropFilter = `blur(${parseInt(myChoice.value)}px)`
+        if(myChoice.value < 10){
+            illustration.style.display = "none"
+            surprise.style.display = "flex"
             blurry.style.backdropFilter = "blur(0px)" 
+
+            body.style.backgroundColor = "white"
+            container.style.backgroundColor = "white"
         }
 })
